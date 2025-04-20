c7524c8a68e09e33a779e7614642cd21b6a8bf0d
Goohyun3436 <3436hyun@gmail.com>
2025-04-20 16:04:46 +0900

    Initial Commit

---
+++ ios-coin-pedia-swiftui/ios_coin_pedia_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_coin_pedia_swiftuiApp.swift
+//  ios-coin-pedia-swiftui
+//
+//  Created by Goo on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_coin_pedia_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
